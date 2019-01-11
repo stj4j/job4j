532d8bcd0b6411f8c41978f016651a59d6a7aad3
Sergii Telegin <sergii.telegin@gmail.com>
2019-01-10 22:02:06 -0800

    1.3. JavDoc. Code convetion.[#104099]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,10 +1,19 @@
-
+/**
+* Package for calculate task
+*
+* @author Sergii (example@scam.com)
+* @version $id$
+* @since 0.1
+*/
 package ru.job4j.calculate;
 
-public class Calculate
-	{
-		public static void main(String[] args)
-		{
+public class Calculate{
+	
+	/**
+	*	main will print message
+	*/
+	
+	public static void main(String[] args){
 			System.out.println("Hello world!");
 		}
 	}
