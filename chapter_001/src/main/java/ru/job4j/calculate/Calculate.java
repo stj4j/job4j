96a13e4e029deadbb99764c687efcc987544a4e7
Sergii Telegin <sergii.telegin@gmail.com>
2019-01-10 23:35:16 -0800

    1.5 Checkstyle [#104101]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -7,14 +7,14 @@
 */
 package ru.job4j.calculate;
 
-public class Calculate{
+public class Calculate {
 	
 	/**
 	*	main will print message
 	*	@param args - args.
 	*/
 	
-	public static void main(String[] args){
+	public static void main(String[] args) {
 			System.out.println("Hello world!");
 		}
 
