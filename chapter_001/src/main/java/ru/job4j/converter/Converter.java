cd73091888b43ef85b7918903954a01b4ec60604
Sergii Telegin <sergii.telegin@gmail.com>
2019-01-11 23:49:20 -0800

    3.2. Конвертер валюты. [#104087]

---
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,40 @@
+package ru.job4j.converter;
+
+
+public class Converter {
+
+    /**
+     * Конвертируем рубли в евро.
+     * @param value рубли.
+     * @return Евро.
+     */
+    public int rubleToEuro(int value) {
+       return value*70;
+    }
+
+    /**
+     * Конвертируем рубли в доллары.
+     * @param value рубли.
+     * @return Доллары
+     */
+    public int rubleToDollar(int value) {
+        return value * 60;
+    }
+    /**
+     * Конвертируем рубли в евро.
+     * @param value рубли.
+     * @return Евро.
+     */
+    public int euroToRuble(int value) {
+        return value/70;
+    }
+
+    /**
+     * Конвертируем рубли в доллары.
+     * @param value рубли.
+     * @return Доллары
+     */
+    public int dollarToRuble(int value) {
+        return value/60;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConverterTest {
+
+    @Test
+    public void when60RubleToDollarThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToDollar(60);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void when70RubleToEuroThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToEuro(70);
+        assertThat(result, is(1));
+    }
+
+    @Test
+    public void when100ollarsToDRubleThen600() {
+        Converter converter = new Converter();
+        int result = converter.dollarToRuble(100);
+        assertThat(result, is(600));
+    }
+
+    @Test
+    public void when100EuroToRubleThen700() {
+        Converter converter = new Converter();
+        int result = converter.euroToRuble(100);
+        assertThat(result, is(700));
+    }
+
+}
