4dc8768558a44f10fc8dfef7d4369c9a23dac1c9
Sergii Telegin <sergii.telegin@gmail.com>
2019-01-14 08:40:30 -0800

    5.4. Построить пирамиду в псевдографике. [#104073]

---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import java.util.StringJoiner;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author Sergii (info@scam.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+
+public class PaintTest {
+
+    @Test
+    public void pyramid() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()
+                )
+        );
+    }
+}
