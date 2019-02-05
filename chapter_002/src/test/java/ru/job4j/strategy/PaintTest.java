3bb8c567e9488d282cfcdb7628e0b96c3d69fd19
Sergii Telegin <sergii.telegin@gmail.com>
2019-02-04 20:52:39 -0800

    5. Рефакторинг теста @Before @After [104134]

--- chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -7,11 +9,23 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PaintTest {
- @Test
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
+   @Test
    public void whenDrawSquare() {
-     PrintStream stdout = System.out;
-     ByteArrayOutputStream out = new ByteArrayOutputStream();
-     System.setOut(new PrintStream(out));
      new Paint().paint(new Square());
      assertThat(
              new String(out.toByteArray()),
@@ -26,20 +40,11 @@ public class PaintTest {
                              .toString()
              )
      );
-     System.setOut(stdout);
-    }
+   }
 
     @Test
     public void whenDrawTriangle() {
-        // получаем ссылку на стандартный вывод в консоль.
-        PrintStream stdout = System.out;
-        // Создаем буфур для хранения вывода.
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-        System.setOut(new PrintStream(out));
-        // выполняем действия пишушиее в консоль.
         new Paint().paint(new Triangle());
-        // проверяем результат вычисления
         assertThat(
                 new String(out.toByteArray()),
                 is(
@@ -53,8 +58,6 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        // возвращаем обратно стандартный вывод в консоль.
-        System.setOut(stdout);
     }
 }
 
