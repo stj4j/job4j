15713b054d9df2949740713a25c8d104743ab944
Sergii Telegin <sergii.telegin@gmail.com>
2019-03-09 02:15:34 -0800

    clean

--- chess/src/main/java/ru/job4j/chess/Figure.java
+++ chess/src/main/java/ru/job4j/chess/Figure.java
@@ -16,7 +16,7 @@ public abstract class Figure implements ru.job4j.chess.firuges.Figure {
     public Cell[] way(Cell source, Cell dest) {
         Cell[] steps = new Cell[0];
         if (source.y == dest.y + 1 && source.x == dest.x) {
-            steps = new Cell[] { dest };
+            steps = new Cell[] {dest};
         }
         return steps;
     }
