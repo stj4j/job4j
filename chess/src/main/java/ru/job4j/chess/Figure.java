a5da94e75f442d252b0ce835be04192dd5b97c73
Sergii Telegin <sergii.telegin@gmail.com>
2019-03-09 01:31:57 -0800

    chess game

---
+++ chess/src/main/java/ru/job4j/chess/Figure.java
@@ -0,0 +1,24 @@
+package ru.job4j.chess;
+
+import ru.job4j.chess.firuges.Cell;
+
+/**
+ * Created by Sergii.
+ */
+public abstract class Figure implements ru.job4j.chess.firuges.Figure {
+    final Cell position;
+
+
+    protected Figure(Cell position) {
+        this.position = position;
+    }
+
+    public Cell[] way(Cell source, Cell dest) {
+        Cell[] steps = new Cell[0];
+        if (source.y == dest.y + 1 && source.x == dest.x) {
+            steps = new Cell[] { dest };
+        }
+        return steps;
+    }
+
+}
