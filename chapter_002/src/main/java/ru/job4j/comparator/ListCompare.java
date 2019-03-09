4f9924da866c3298b34ffb389c7fa6bcd7fd86be
Sergii Telegin <sergii.telegin@gmail.com>
2019-03-09 00:59:39 -0800

    3. Компаратор для строк. [104113]

---
+++ chapter_002/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -0,0 +1,43 @@
+package ru.job4j.comparator;
+
+import java.util.Comparator;
+
+/**
+ * Created by Sergii.
+ */
+public class ListCompare implements Comparator<String> {
+
+    // ascii A-Z 65-90
+    // ascii a-z 97-122
+
+    @Override
+    public int compare(String o1, String o2) {
+
+
+
+        char[] first = o1.toCharArray();
+        char[] second = o2.toCharArray();
+        int rezult = 0;
+
+        for (int i = 0; i < minArrayLenght(first.length, second.length - 1); i++) {
+            if ((int) first[i] > (int) second[i]) {
+                rezult = 1;
+                break;
+            }
+            if ((int) first[i] < (int) second[i]) {
+                rezult = -1;
+                break;
+            }
+        }
+
+        if (rezult == 0 && first.length != second.length) {
+            rezult = first.length > second.length ? 1 : -1;
+        }
+
+        return rezult;
+    }
+
+    private int minArrayLenght(int f, int s) {
+        return f > s ? f : s;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/comparator/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by sergii on 3/9/19.
+ */
+package ru.job4j.comparator;
