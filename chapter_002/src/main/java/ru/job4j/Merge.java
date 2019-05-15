39c7972d7a1e1e9453fbba083fc663283636bda4
Sergii Telegin <sergii.telegin@gmail.com>
2019-05-14 19:35:12 -0700

    New arrays merge + sort  [#104106]

--- chapter_002/src/main/java/ru/job4j/Merge.java
+++ chapter_002/src/main/java/ru/job4j/Merge.java
@@ -1,25 +1,45 @@
 package ru.job4j;
-
-import java.util.Arrays;
-
 /**
  * Created by Sergii.
  */
 public class Merge {
 
     public int[] merge(int[] left, int[] right) {
-        int[] rsl = new int[left.length + right.length];
+        int newarraysize = left.length + right.length;
+        int[] rsl = new int[newarraysize];
+        int i = 0;
+        int leftindex = 0;
+        int rightindex = 0;
 
-        int count = 0;
+        while (i < newarraysize) {
+            if ((leftindex < left.length) && (rightindex < right.length)) {
+                if (left[leftindex] < right[rightindex]) {
+                    rsl[i] = left[leftindex];
+                    i++;
+                    leftindex++;
+                } else {
+                    rsl[i] = right[rightindex];
+                    i++;
+                    rightindex++;
+                }
+            } else {
+                if (leftindex >= left.length) {
+                    while (rightindex < right.length) {
+                        rsl[i] = right[rightindex];
+                        i++;
+                        rightindex++;
+                    }
+                }
+                if (rightindex >= right.length) {
+                    while (leftindex < left.length) {
+                        rsl[i] = left[leftindex];
+                        leftindex++;
+                        i++;
+                    }
+                }
+            }
 
-        for(int i = 0; i < left.length; i++) {
-            rsl[i] = left[i];
-            count++;
         }
-        for(int j = 0; j < right.length;j++) {
-            rsl[count++] = right[j];
-        }
-        Arrays.sort(rsl);
-        return rsl;
+    return rsl;
     }
 }
